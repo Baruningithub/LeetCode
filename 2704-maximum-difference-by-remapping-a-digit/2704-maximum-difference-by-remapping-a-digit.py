8814007bbd9ffaf82c81afacef7b0be1ed98efda
Barun Hazary <95410218+Baruningithub@users.noreply.github.com>
2025-06-14 21:16:46 +0530

    Time: 0 ms (100%), Space: 17.8 MB (29.44%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        s = str(num)
+        max_str = str()
+        min_str = str()
+        for i in s:
+            if i == '9':
+                max_str += i
+            else:
+                max_str = s.replace(i,'9')
+                break
+
+        min_str = s.replace(s[0],'0')
+
+        return int(max_str) - int(min_str)
+        
