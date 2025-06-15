1517ed2a9d3298d4c4c51f154c8529e786628aee
Barun Hazary <95410218+Baruningithub@users.noreply.github.com>
2025-06-15 12:52:45 +0530

    Time: 0 ms (100%), Space: 17.9 MB (12.2%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
@@ -3,12 +3,13 @@ class Solution:
         s = str(num)
         max_str = str()
         min_str = str()
+        
         for i in s:
-            if i == '9':
-                max_str += i
-            else:
+            if i != '9':
                 max_str = s.replace(i,'9')
                 break
+            else:
+                max_str = s
 
         min_str = s.replace(s[0],'0')
 
