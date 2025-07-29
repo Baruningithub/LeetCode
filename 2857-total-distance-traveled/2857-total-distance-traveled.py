3787f05e01616fb26f1efe4da973d79ca7e95e3d
Barun Hazary <95410218+Baruningithub@users.noreply.github.com>
2025-07-29 18:53:26 +0530

    Time: 9 ms (51.46%), Space: 17.7 MB (87.96%) - LeetHub

--- 2857-total-distance-traveled/2857-total-distance-traveled.py
+++ 2857-total-distance-traveled/2857-total-distance-traveled.py
@@ -1,13 +1,20 @@
+# class Solution:
+#     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
+#         # additional = min(additionalTank, mainTank // 5)
+#         # ans = (mainTank + additional) * 10
+#         # return ans
+#         total = 0
+#         while additionalTank > 0 and mainTank >= 5:
+#             mainTank -= 4
+#             additionalTank -= 1
+#             total += 5
+
+#         total += mainTank
+#         return total*10
+
 class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
-        # additional = min(additionalTank, mainTank // 5)
-        # ans = (mainTank + additional) * 10
-        # return ans
-        total = 0
-        while additionalTank > 0 and mainTank >= 5:
-            mainTank -= 4
-            additionalTank -= 1
-            total += 5
-
-        total += mainTank
-        return total*10
+        distance = mainTank*10
+        maxRefils = (mainTank-1)//4
+        distance += min(maxRefils,additionalTank)*10
+        return distance
