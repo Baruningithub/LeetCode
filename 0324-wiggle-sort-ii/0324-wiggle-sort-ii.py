e0c09c6bd7d2d281f9762bdf499a2e0c7b071ad7
Barun Hazary <95410218+Baruningithub@users.noreply.github.com>
2025-07-30 12:55:49 +0530

    Time: 6 ms (64.13%), Space: 19.2 MB (76.81%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -1,13 +1,17 @@
 class Solution:
     def wiggleSort(self, nums: List[int]) -> None:
-        n = len(nums)
-        sorted_nums = sorted(nums)
-        mid = (n + 1) // 2  # right half can be same length or smaller
-        # split into two halves and reverse for easier pop from end
-        left = sorted_nums[:mid][::-1]
-        right = sorted_nums[mid:][::-1]
-        for i in range(n):
-            if i % 2 == 0:
-                nums[i] = left.pop(0)
-            else:
-                nums[i] = right.pop(0)
+        # n = len(nums)
+        # sorted_nums = sorted(nums)
+        # mid = (n + 1) // 2  # right half can be same length or smaller
+        # # split into two halves and reverse for easier pop from end
+        # left = sorted_nums[:mid][::-1]
+        # right = sorted_nums[mid:][::-1]
+        # for i in range(n):
+        #     if i % 2 == 0:
+        #         nums[i] = left.pop(0)
+        #     else:
+        #         nums[i] = right.pop(0)
+        nums.sort()
+        mid = (len(nums) - 1) // 2
+        left, right = nums[mid::-1], nums[:mid:-1]
+        nums[::2], nums[1::2] = left, right
