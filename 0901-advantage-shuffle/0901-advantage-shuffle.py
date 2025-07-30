bc1fd5c8a7af658e9406486a3ce283e36436deeb
Barun Hazary <95410218+Baruningithub@users.noreply.github.com>
2025-07-30 12:10:38 +0530

    Time: 115 ms (83.25%), Space: 40.8 MB (75%) - LeetHub

---
+++ 0901-advantage-shuffle/0901-advantage-shuffle.py
@@ -0,0 +1,17 @@
+class Solution:
+    def advantageCount(self, nums1, nums2):
+        n = len(nums1)
+        nums1_sorted = sorted(nums1)
+        nums2_sorted = sorted([(num, i) for i, num in enumerate(nums2)])
+        
+        ans = [0] * n
+        lo, hi = 0, n - 1
+        
+        for num, idx in reversed(nums2_sorted):
+            if nums1_sorted[hi] > num:
+                ans[idx] = nums1_sorted[hi]
+                hi -= 1
+            else:
+                ans[idx] = nums1_sorted[lo]
+                lo += 1
+        return ans
