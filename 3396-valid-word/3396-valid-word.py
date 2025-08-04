a10b175036c74775f57d4318beacf6edc142441c
Barun Hazary <95410218+Baruningithub@users.noreply.github.com>
2025-08-04 11:54:36 +0530

    Time: 3 ms (17.09%), Space: 17.7 MB (55.72%) - LeetHub

--- 3396-valid-word/3396-valid-word.py
+++ 3396-valid-word/3396-valid-word.py
@@ -1,5 +1,5 @@
 import re
 class Solution:
     def isValid(self, word: str) -> bool:
-        regex = '(?=.*[aeiou])(?=.*[^\daeiou])\w{3,}$'
-        return re.match(regex, word, I) is not None
+        regex = r'^(?=.*[AEIOUaeiou])(?=.*[B-DF-HJ-NP-TV-Zb-df-hj-np-tv-z])[A-Za-z0-9]{3,}$'
+        return re.match(regex, word) is not None
