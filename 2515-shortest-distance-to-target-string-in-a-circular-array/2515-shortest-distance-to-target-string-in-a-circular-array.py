2bd8814be630a9e07244eeb5c8d37c081f51197f
Barun Hazary <95410218+Baruningithub@users.noreply.github.com>
2026-04-15 13:09:36 +0530

    Time: 0 ms (100%), Space: 19.4 MB (52.69%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -9,8 +9,8 @@ class Solution:
         for i in range (n):
             if words[i] == target:
                 distance = min(distance , 
-                            # minimum distance for current match (as there coulbe multiple matches)
-                            min(abs(i-startIndex), # liner distance
+                            # minimum distance for current match (as there could be multiple matches)
+                            min(abs(i-startIndex), # linear distance
                             n - abs(i-startIndex))) #circular distance
         
         return -1 if distance > n else distance
